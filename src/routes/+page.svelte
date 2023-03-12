22459e7a11c208e2db1169994ead44f6e4cbe19a
kn0wn <59017897+kn0wn@users.noreply.github.com>
2023-03-12 14:04:44 +0000

    fix: changed input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,12 @@
 
 <div class="grid place-items-center">
 	<h1
-		class="font-display text-9xl text-center bg-clip-text text-transparent bg-gradient-to-b from-black to-gray-500"
+		class="font-display mb-4 hidden sm:block text-9xl text-center bg-clip-text text-transparent bg-gradient-to-b from-black to-gray-500"
+	>
+		Barrier.studio
+	</h1>
+	<h1
+		class="font-display mb-4 text-7xl sm:hidden text-center bg-clip-text text-transparent bg-gradient-to-b from-black to-gray-500"
 	>
 		Barrier.studio
 	</h1>
@@ -14,14 +19,16 @@
 
 		<div class="flex gap-x-2">
 			<input
-				class="border-t-black border-l-black border-2 border-b-gray-400 border-r-gray-400"
+				class="border-t-black placeholder:font-display border-l-black border-2 border-b-gray-400 border-r-gray-400"
 				type="email"
 				name="email"
 				value={email}
+				placeholder="john@doe.com"
 			/>
 			<button
+				disabled={!email}
 				type="submit"
-				class="px-2 bg-gray-900 text-white font-display border-l-2 border-t-2 border-gray-500"
+				class="px-4 bg-gray-900 text-white font-display border-l-2 border-t-2 border-gray-500"
 			>
 				Submit
 			</button>
